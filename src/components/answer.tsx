d6f3e1da48620a8957b0800cd63bdee937f17ec4
chasen2021 <50192480+tortooga2@users.noreply.github.com>
2025-05-11 12:00:25 -0700

    added vote area to answer.tsx. happy that component is a drag and drop type deal

--- src/components/answer.tsx
+++ src/components/answer.tsx
@@ -1,9 +1,12 @@
+import VoteArea from "./voteArea";
+
 export default async function Answer({ answer }) {
     return (
         <div>
             <p>{answer.userId}</p>
             <p>{answer.createdAt.toString()}</p>
             <p>{answer.answer}</p>
+            <VoteArea postId={answer.id} postType={"answer"} />
         </div>
     );
 }
